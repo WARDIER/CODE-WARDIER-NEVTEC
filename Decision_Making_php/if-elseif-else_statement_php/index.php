5cc335e0891c8f7481b17aab696fcc6d187da23a
Hamza Aqeel <139615197+WARDIER@users.noreply.github.com>
2023-08-05 22:08:23 +0500

    NAVTTC_PHP

---
+++ Decision_Making_php/if-elseif-else_statement_php/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$day = "Tuesday";
+
+if ($day == "Monday") {
+    echo "It's the start of the week.";
+} elseif ($day == "Friday") {
+    echo "It's the end of the week.";
+} else {
+    echo "It's a regular weekday.";
+}
+
+
+
+
+?>
---
+++ Decision_Making_php/if_statement_php/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$age = 25;
+
+if ($age >= 18) {
+    echo "You are an adult.";
+}
+
+
+
+
+?>
