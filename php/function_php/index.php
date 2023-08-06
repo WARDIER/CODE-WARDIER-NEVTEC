f8f44ac2b5f60a5453db5419b8d517860f6a5f00
Hamza Aqeel <139615197+WARDIER@users.noreply.github.com>
2023-08-06 11:15:24 +0500

    NAVTTC_PHP

---
+++ php/function_php/index.php
@@ -0,0 +1,10 @@
+<?php
+function addNumbers($num1, $num2) {
+    $sum = $num1 + $num2;
+    return $sum;
+}
+
+$result = addNumbers(5, 10); // Call the function with arguments 5 and 10
+echo $result; 
+
+?>
